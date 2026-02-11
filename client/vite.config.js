b02480cc10249ac7626cd922e196fa0d18aae58b
Ekbal Hossen <skme4071@gmail.com>
2026-02-11 21:41:57 +0600

    ok

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  root: 'client', // client ফোল্ডারকে root হিসেবে দেখানো
+  plugins: [react()],
+  build: {
+    outDir: '../dist', // server বা public ফোল্ডারে output
+  },
+})
